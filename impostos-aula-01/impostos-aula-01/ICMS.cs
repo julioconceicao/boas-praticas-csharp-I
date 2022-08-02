79b3f785cb42f4902df8425a19526612ef91adff
Julio César G. Conceição <juliocesar.conceicao@novahaus.com.br>
2022-08-02 16:29:13 -0300

    Up

--- impostos-aula-01/impostos-aula-01/ICMS.cs
+++ impostos-aula-01/impostos-aula-01/ICMS.cs
@@ -8,7 +8,7 @@ namespace impostos_aula_01
     {
         public double Calcula(Orcamento orcamento)
         {
-            return orcamento.Valor * 0.1;
+            return (orcamento.Valor * 0.05) + 50.0;
         }
     }
 }

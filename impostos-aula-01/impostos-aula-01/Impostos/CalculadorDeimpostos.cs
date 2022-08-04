4bb813ca0573e4459f8f66d30e4e0a5536f23a3d
Julio César G. Conceição <juliocesar.conceicao@novahaus.com.br>
2022-08-03 22:29:04 -0300

    feat: Impostos

---
+++ impostos-aula-01/impostos-aula-01/Impostos/CalculadorDeimpostos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace impostos_aula_01.Impostos
+{
+    public class CalculadorDeimpostos
+    {
+        public void RealizaCalculo(Orcamento orcamento, IImposto imposto)
+        {
+            double resultado = imposto.Calcula(orcamento);
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ impostos-aula-01/impostos-aula-01/Impostos/ICCC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace impostos_aula_01.Impostos
+{
+    public class ICCC : IImposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            if (orcamento.Valor < 1000.0)
+            {
+                return orcamento.Valor * 0.05;
+            }
+            else if (orcamento.Valor >= 1000.0 && orcamento.Valor <= 3000.0)
+            {
+                return orcamento.Valor * 0.07;
+            }
+            else
+            {
+                return orcamento.Valor * 0.08 + 30.0;
+            }
+        }
+    }
+}

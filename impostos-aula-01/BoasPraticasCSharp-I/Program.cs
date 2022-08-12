12fbdf4e236268f840bd0492ec3e85444274f7df
Julio César G. Conceição <juliocesar.conceicao@novahaus.com.br>
2022-08-12 10:46:03 -0300

    .

--- impostos-aula-01/BoasPraticasCSharp-I/Program.cs
+++ impostos-aula-01/BoasPraticasCSharp-I/Program.cs
@@ -1,14 +1,22 @@
-﻿using impostos_aula_01.Descontos;
-using impostos_aula_01.Impostos;
-using System;
+﻿// using impostos_aula_01.Descontos;
+// using impostos_aula_01.Impostos;
+// using System;
 
-namespace impostos_aula_01
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-        }
-    }
-}
+// namespace impostos_aula_01
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Imposto iss = new ISS(new ICMS());
+
+//             Orcamento orcamento = new Orcamento(500);
+
+//             double valor = iss.Calcula(orcamento);
+
+//             Console.WriteLine(valor);
+
+//             Console.ReadKey();
+//         }
+//     }
+// }
